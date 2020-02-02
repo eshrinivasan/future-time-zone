9f0850143746e07ce16a60a6cbfffbccc488d6a0
Shrinivasan Neelamegam <shrinivasan.neelamegam@sap.com>
2020-02-02 19:57:49 +0530

    Fix bug with time display on zone dropdown change

--- batchscript.sh
+++ batchscript.sh
@@ -1,5 +1,5 @@
 ng build --prod --output-path docs --base-href /future-time-zone/
 git add .
-git commit -m "Fix bug with time formatting "
+git commit -m "Fix bug with time display on zone dropdown change "
 git push origin master
 ng deploy
--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -27,30 +27,30 @@ export class DisplayComponent implements OnInit {
   }
   constructor() {
   	this.options = { weekday: 'short', year: 'numeric', month: 'short', day: '2-digit', hour:'numeric', minute: 'numeric'};
-	this.world_timezones = [
-      'Australia/Sydney',
-      'Asia/Kolkata',
-      'Asia/Kuala_Lumpur',
-      'Asia/Singapore',
-      'Asia/Seoul',
-      'Asia/Hong_Kong',
-      'Asia/Tehran',
-      'Asia/Ho_Chi_Minh',
-      'Europe/Brussels',
-      'Europe/Prague',
-      'Europe/Paris',
-      'Europe/London',
-      'Europe/Sofia',
-      'Europe/Budapest',
-      'Europe/Dublin',
-      'Europe/Berlin',
-      'America/Chicago',
-      'America/Toronto',
-      'America/Vancouver',
-      'America/New_York',
-      'America/Los_Angeles',  
-      'America/Mexico_City'
-    ];
+  	this.world_timezones = [
+        'Australia/Sydney',
+        'Asia/Kolkata',
+        'Asia/Kuala_Lumpur',
+        'Asia/Singapore',
+        'Asia/Seoul',
+        'Asia/Hong_Kong',
+        'Asia/Tehran',
+        'Asia/Ho_Chi_Minh',
+        'Europe/Brussels',
+        'Europe/Prague',
+        'Europe/Paris',
+        'Europe/London',
+        'Europe/Sofia',
+        'Europe/Budapest',
+        'Europe/Dublin',
+        'Europe/Berlin',
+        'America/Chicago',
+        'America/Toronto',
+        'America/Vancouver',
+        'America/New_York',
+        'America/Los_Angeles',  
+        'America/Mexico_City'
+      ];
   }
   //choose city using select dropdown and update the time for that zone
   changeCity(e) {
@@ -58,10 +58,8 @@ export class DisplayComponent implements OnInit {
       onlySelf: true
     });
     this.selectedCity = this.cityName.value.split(' ')[1];
-    this.zone = new Date(this.date).toLocaleString("en-US", this._merge(this.options, {timeZone: this.selectedCity}));
+    this.zone = new Date().toLocaleString("en-US", this._merge(this.options, {timeZone: this.selectedCity}));
     this.date = new Date(this.zone);
-    
-    //this.messageEvent.emit(this.date);
   }
   _merge(ob1, ob2){
     let target = Object.assign(ob1, ob2);
