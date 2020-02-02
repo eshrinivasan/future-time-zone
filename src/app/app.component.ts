c33f8fe4e15269dbf32ef027a1cc0a52a39fc6d1
Shrinivasan Neelamegam <shrinivasan.neelamegam@sap.com>
2020-02-02 17:39:58 +0530

    changed logic to make it more dynamic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,31 +8,40 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
   europe: string;
   america: string;
+  india: string;
   date: Date = new Date();
   options: any;
-  settings = {
-      bigBanner: true,
-      timePicker: true,
-      format: 'EEE, MMM dd, yyyy, hh:mm a',
-      defaultOpen: false
-  }
   constructor(){
-     this.options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour:'numeric', minute: 'numeric'};
+     this.options = { weekday: 'short', year: 'numeric', month: 'short', day: '2-digit', hour:'numeric', minute: 'numeric'};
      //on page load, display current time
+     this.india = new Date().toLocaleString("en-US", this._merge(this.options, {timeZone: "Asia/Kolkata"}));
      this.europe = new Date().toLocaleString("en-US", this._merge(this.options, {timeZone: "Europe/Berlin"}));
      this.america = new Date().toLocaleString("en-US", this._merge(this.options, {timeZone: "America/New_York"}));
   }
   _merge(ob1, ob2){
-    let target = Object.assign(ob1, ob2);
-    return target;
+     let target = Object.assign(ob1, ob2);
+     return target;
   }
   ngOnInit(){
     
   }
-  onDateSelect(){
-      //on date select, display corresponding time depending on zone
-  	var localdate = this.date;
-     this.europe = new Date(localdate).toLocaleString("en-US", this._merge(this.options, {timeZone: "Europe/Berlin"}));
-     this.america = new Date(localdate).toLocaleString("en-US", this._merge(this.options, {timeZone: "America/New_York"}));
+  receiveMessage($event){
+     this.india = this.changeTimezone($event, 'Asia/Kolkata');
+     this.europe = this.changeTimezone($event, 'Europe/Berlin');
+     this.america = this.changeTimezone($event, 'America/New_York');
+  }
+
+  changeTimezone(evt, ianatz) {
+       var invdate = new Date(evt.zoneDate).toLocaleString('en-US', {
+         timeZone: ianatz
+       });
+
+       var selCitydate = new Date(evt.zoneDate).toLocaleString('en-US', {
+         timeZone: evt.city
+       });
+
+       var diff = new Date(selCitydate).getTime() - new Date(invdate).getTime();
+       var calculatedDate = new Date(new Date(evt.zoneDate).getTime() - diff);
+       return calculatedDate.toLocaleString("en-US",this._merge(this.options, {timeZone: ianatz}))
   }
 }
--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 @Component({
@@ -7,18 +7,29 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./display.component.css']
 })
 export class DisplayComponent implements OnInit {
-  @Input() date: string;
+  //@Output() date: string;
+  @Output() messageEvent = new EventEmitter<object>();
+  date: Date = new Date();
   cityName = new FormControl('');
   zone: string;
+  newZone: string;
   selectedCity: string;
   options: any;
   world_timezones: any;
-
+  europe: string;
+  america: string;
+  india:string;
+  settings = {
+      bigBanner: true,
+      timePicker: true,
+      format: 'EEE, MMM dd, yyyy, hh:mm a',
+      defaultOpen: false
+  }
   constructor() {
-  	this.options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour:'numeric', minute: 'numeric'};
+  	this.options = { weekday: 'short', year: 'numeric', month: 'short', day: '2-digit', hour:'numeric', minute: 'numeric'};
 	this.world_timezones = [
       'Australia/Sydney',
-      'Asia/Shanghai',
+      'Asia/Kolkata',
       'Asia/Kuala_Lumpur',
       'Asia/Singapore',
       'Asia/Seoul',
@@ -29,11 +40,11 @@ export class DisplayComponent implements OnInit {
       'Europe/Prague',
       'Europe/Paris',
       'Europe/London',
-      'Europe/Athens',
+      'Europe/Sofia',
       'Europe/Budapest',
       'Europe/Dublin',
-      'Europe/Rome',
-      'Europe/Amsterdam',
+      'Europe/Berlin',
+      'America/Chicago',
       'America/Toronto',
       'America/Vancouver',
       'America/New_York',
@@ -41,24 +52,28 @@ export class DisplayComponent implements OnInit {
       'America/Mexico_City'
     ];
   }
-  // choose city using select dropdown and update the time for that zone
+  //choose city using select dropdown and update the time for that zone
   changeCity(e) {
     this.cityName.setValue(e.target.value, {
       onlySelf: true
     });
-
     this.selectedCity = this.cityName.value.split(' ')[1];
     this.zone = new Date(this.date).toLocaleString("en-US", this._merge(this.options, {timeZone: this.selectedCity}));
+    this.date = new Date(this.zone);
+    
+    //this.messageEvent.emit(this.date);
   }
   _merge(ob1, ob2){
     let target = Object.assign(ob1, ob2);
     return target;
   }
+  onDateSelect(){
+  	this.newZone = new Date(this.date).toISOString();
+  	//on date select, emit changed date-time to parent
+   	this.messageEvent.emit({zoneDate: this.newZone, city: this.selectedCity});
+  }
   ngOnInit(){
 
   }
-  //Look for change in time in the first zone, reflect changes in this zone as well
-  ngOnChanges(changes: SimpleChanges) {
-  	this.zone = new Date(changes['date'].currentValue).toLocaleString("en-US", this._merge(this.options, {timeZone: this.selectedCity}));
-  }
+ 
 }
