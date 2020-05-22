0c6dadb27b2f6aaccc2282d2a0c133c32e4beb1f
Shrinivasan Neelamegam <shrinivasan.neelamegam@sap.com>
2020-05-22 19:44:19 +0530

    added new features. updated readme

--- batchscript.sh
+++ batchscript.sh
@@ -1,5 +1,5 @@
 ng build --prod --output-path docs --base-href /future-time-zone/
 git add .
-git commit -m "added google analytics"
+git commit -m "added new features. updated readme"
 git push origin master
 ng deploy
