b3769a662ea29e3aaf38fb82184a95926eb42818
Shrinivasan Neelamegam <shrinivasan.neelamegam@sap.com>
2020-05-22 19:37:27 +0530

    Added paypal smart buttons checkout

--- server.js
+++ server.js
@@ -13,4 +13,6 @@ res.sendFile(path.join(__dirname,'/dist/future-time-zone/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8082, function(){
+    console.log("server up on port 8082");
+});
